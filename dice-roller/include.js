7d81e37f9bfe2bb5b5aff8dbcf7ff4f02d3173eb
stormhaul <stormhaul@users.noreply.github.com>
2019-03-11 16:35:30 -0500

    Added includer and landing page, to begin debugging existing code.

---
+++ dice-roller/include.js
@@ -0,0 +1,27 @@
+let includes = [
+  'Helpers/angle-math.js',
+  'Helpers/vector-math.js',
+  'Helpers/coordiate-math.js',
+  'Instructions/Rendor/circle.js',
+  'Instructions/Rendor/number.js',
+  'Instructions/Rendor/regular-polygon.js',
+  'die.js',
+  'face.js',
+  'pool.js'
+];
+
+loadScripts(includes);
+
+function dynamicallyLoadScript(url) {
+    var script = document.createElement("script");  // create a script DOM node
+    script.src = url;  // set its src to the provided URL
+
+    document.head.appendChild(script);  // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+}
+
+function loadScripts(scripts) {
+  let directory = 'dice-roller/';
+  for (let i in scripts) {
+    dynamicallyLoadScript(directory + scripts[i]);
+  }
+}
