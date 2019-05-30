de156cc8046c0cc643ec764e8909915ab97ebc56
Timothy Smith <tsmith@mbira.com>
2019-05-30 14:27:01 -0500

    Added git ignore and skills list.

---
+++ tabletop/src/Globals/skills-list.js
@@ -0,0 +1,50 @@
+var SKILLS = {
+    all: [
+        'Acrobatics',
+        'Animal Handling',
+        'Athletics',
+        'Deception',
+        'History',
+        'Insight',
+        'Intimidation',
+        'Investigation',
+        'Medicine',
+        'Nature',
+        'Perception',
+        'Performance',
+        'Persuasion',
+        'Religion',
+        'Sleight of Hand',
+        'Stealth',
+        'Survival'
+    ],
+    Strength: [
+        'Athletics',
+        'Dexterity'
+    ],
+    Dexterity: [
+        'Acrobatics',
+        'Sleight of Hand',
+        'Stealth'
+    ],
+    Intelligence: [
+        'Arcana',
+        'History',
+        'Investigation',
+        'Nature',
+        'Religion'
+    ],
+    Wisdom: [
+        'Animal Handling',
+        'Insight',
+        'Medicine',
+        'Perception',
+        'Survival'
+    ],
+    Charisma: [
+        'Deception',
+        'Intimidation',
+        'Performance',
+        'Persuasion'
+    ]
+};
