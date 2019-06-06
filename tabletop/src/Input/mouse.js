f622a7ae6863c8acce977159684f8b6452ab7cd8
Timothy Smith <tsmith@mbira.com>
2019-06-06 13:26:47 -0500

    Made click types constants.

--- tabletop/src/Input/mouse.js
+++ tabletop/src/Input/mouse.js
@@ -1,23 +1,26 @@
 "use strict";
 
+const LEFTCLICK = "left-click";
+const MIDDLECLICK = "middle-click";
+const RIGHTCLICK = "right-click";
+
 function Mouse() {
     let that = this;
-    this.clickConstants = {
-        "left-click": 1,
-        "middle-click": 2,
-        "right-click": 3
-    };
+    this.clickConstants = {};
+    this.clickConstants[LEFTCLICK] = 1;
+    this.clickConstants[MIDDLECLICK] = 2;
+    this.clickConstants[RIGHTCLICK] = 3;
 
     document.addEventListener("mouseup", function(e) {
         switch(e.which) {
-            case that.clickConstants["left-click"]:
-                console.log("left click up");
+            case that.clickConstants[LEFTCLICK]:
+                console.log(LEFTCLICK + " up");
                 break;
-            case that.clickConstants["middle-click"]:
-                console.log("middle click up");
+            case that.clickConstants[MIDDLECLICK]:
+                console.log(MIDDLECLICK + " up");
                 break;
-            case that.clickConstants["right-click"]:
-                console.log("right click up");
+            case that.clickConstants[RIGHTCLICK]:
+                console.log(RIGHTCLICK + " up");
                 break;
             default:
                 console.log("Unknown mouse button: " + e.which);
@@ -25,14 +28,14 @@ function Mouse() {
     });
     document.addEventListener("mousedown", function(e) {
         switch(e.which) {
-            case that.clickConstants["left-click"]:
-                console.log("left click down");
+            case that.clickConstants[LEFTCLICK]:
+                console.log(LEFTCLICK + " down");
                 break;
-            case that.clickConstants["middle-click"]:
-                console.log("middle click down");
+            case that.clickConstants[MIDDLECLICK]:
+                console.log(MIDDLECLICK + " down");
                 break;
-            case that.clickConstants["right-click"]:
-                console.log("right click down");
+            case that.clickConstants[RIGHTCLICK]:
+                console.log(RIGHTCLICK + " down");
                 break;
             default:
                 console.log("Unknown mouse button: " + e.which);
